1dfaf083de8e1656561fa391d46a375be0ac246c
Parker Bedlan <parkerbedlan@gmail.com>
2025-03-28 18:04:02 -0400

    open-webui running as a service

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -29,4 +29,23 @@
     # ocl-icd
     # gpu-viewer
   ];
+
+  # programs.nix-ld = {
+  #   enable = true;
+  #   libraries = with pkgs; [
+  #   ];
+  # };
+
+  # hosted on localhost:8080
+  services.open-webui = {
+    package = pkgs.open-webui; # pkgs must be from stable, for example nixos-24.11
+    enable = true;
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+      OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+    };
+  };
 }
