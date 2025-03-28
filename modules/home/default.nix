0ae4ac807e52f2dfd8ea8f2358039e8ef1d5bccd
Parker Bedlan <parkerbedlan@gmail.com>
2025-03-28 12:46:51 -0400

    a

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -34,6 +34,11 @@
       (pkgs.writeShellScriptBin "my-hello" ''
         echo "Hello, ${config.home.username}!"
       '')
+      (pkgs.writeShellScriptBin "yt" ''
+        pushd ~/Music
+        yt-dlp -x --audio-format mp3 https://www.youtube.com/watch?v=$1
+        popd
+      '')
     ];
 
     home.file = {
@@ -45,6 +50,8 @@
             send-keys 'cd notes' Enter 'gp' Enter 'v' Enter \; \
             new-window -n nixos \; \
             send-keys 'cd nixos' Enter 'gp' Enter 'v' Enter \; \
+            new-window -n nixos \; \
+            send-keys 'cd nixos' Enter \; \
             select-window -t home:1 \; \
             attach-session -t home
       '';
