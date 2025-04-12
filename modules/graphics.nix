6d1cf3f77f7f4bd2656155ebffdabc39c438cc57
Parker Bedlan <parkerbedlan@gmail.com>
2025-04-12 15:16:49 -0400

    using rocmPackages.clr.icd amd driver so that davinci-resolve opens

--- modules/graphics.nix
+++ modules/graphics.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 {
   # formerly hardware.opengl
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [
+      # intel-media-driver
+      # intel-ocl
+      # intel-vaapi-driver
+      rocmPackages.clr.icd
+    ];
   };
 }
