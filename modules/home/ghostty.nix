1e95459595d189731e47a3db8f05ce84c82a6c80
Parker Bedlan <parkerbedlan@gmail.com>
2025-06-06 01:52:18 -0400

    conditional terminal font size based on host

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -1,10 +1,11 @@
-{ ... }:
+{ config, ... }:
 {
   programs.ghostty = {
     enable = true;
     enableBashIntegration = true;
     settings = {
-      font-size = 18;
+      # font-size = if config.host == "desktop" then 18 else if config.host == "laptop" then 14 else null;
+      font-size = if config.host == "desktop" then 18 else 14;
       # https://ghostty.org/docs/config/keybind/reference
       keybind = [
         "ctrl+v=paste_from_clipboard"
