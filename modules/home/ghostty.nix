312913557ed31384e4402e1326237d4ce7efad34
Parker Bedlan <parkerbedlan@gmail.com>
2025-07-05 17:17:31 -0400

    a

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -3,6 +3,7 @@
   programs.ghostty = {
     enable = true;
     enableBashIntegration = true;
+    # https://ghostty.org/docs/config/reference
     settings = {
       # font-size = if config.host == "desktop" then 18 else if config.host == "laptop" then 14 else null;
       font-size = if config.host == "desktop" then 18 else 14;
@@ -12,6 +13,8 @@
       ];
       maximize = true;
       title = "ghostty";
+      # default already?
+      copy-on-select = "clipboard";
     };
   };
 }
