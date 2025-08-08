02b071e1bac7c44b37faa52e45e88abdaea954fd
Parker Bedlan <parkerbedlan@gmail.com>
2025-08-08 18:08:37 -0400

    a

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -48,12 +48,12 @@
 
   # general hosting stuff
   networking = {
-    interfaces.eth0.ipv4.addresses = [
-      {
-        address = "192.168.1.2";
-        prefixLength = 24;
-      }
-    ];
+    # interfaces.eth0.ipv4.addresses = [
+    #   {
+    #     address = "192.168.1.2";
+    #     prefixLength = 24;
+    #   }
+    # ];
     interfaces.wlp9s0.ipv4.addresses = [
       {
         address = "192.168.7.143";
