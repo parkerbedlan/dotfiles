e14623750bd927f3a30ec40be8ca3a2ff69ab95d
Parker Bedlan <parkerbedlan@gmail.com>
2025-08-25 20:58:59 -0400

    a

--- modules/redshift.nix
+++ modules/redshift.nix
@@ -7,8 +7,8 @@
     enable = true;
     # 1000 is warmest, 25000 is coldest
     temperature = {
-      day = 3700; # default 5500
-      night = 2000; # default 3700
+      day = 4000; # default 5500
+      night = 3000; # default 3700
     };
   };
 }
