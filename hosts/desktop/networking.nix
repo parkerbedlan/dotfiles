a1665b1ce551a0f306d86f2c74330bf0c59c806d
Parker Bedlan <parkerbedlan@gmail.com>
2025-09-08 00:56:52 -0400

    a

--- hosts/desktop/networking.nix
+++ hosts/desktop/networking.nix
@@ -18,14 +18,14 @@
       useDHCP = false;
       ipv4.addresses = [
         {
-          address = "192.168.7.143";
+          address = "192.168.4.145";
           prefixLength = 24;
         }
       ];
     };
-    defaultGateway = "192.168.7.1";
+    defaultGateway = "192.168.4.1";
     nameservers = [
-      "192.168.7.1"
+      "192.168.4.1"
       "8.8.8.8"
     ];
   };
@@ -41,13 +41,13 @@
       forwardPorts = [
         {
           sourcePort = 8080;
-          destination = "192.168.7.143:8080"; # Replace with your LAN IP and port
+          destination = "192.168.4.145:8080"; # Replace with your LAN IP and port
           # destination = "0.0.0.0:8080"; # Replace with your LAN IP and port
           proto = "tcp";
         }
         {
           sourcePort = 2283;
-          destination = "192.168.7.143:2283";
+          destination = "192.168.4.145:2283";
           proto = "tcp";
         }
       ];
--- modules/hosts_file.nix
+++ modules/hosts_file.nix
@@ -3,7 +3,7 @@
   networking.extraHosts = ''
     # 0.0.0.0 youtube.com
     # 0.0.0.0 www.youtube.com
-    192.168.7.143 ai.lo
-    192.168.7.143 immich.lo
+    192.168.4.145 ai.lo
+    192.168.4.145 immich.lo
   '';
 }
