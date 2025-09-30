5e4d37dfabd82c88b674c3c069803ec62e1555d6
Parker Bedlan <parkerbedlan@gmail.com>
2025-09-30 16:20:23 -0400

    a

--- modules/nvim/after/plugin/telescope.lua
+++ modules/nvim/after/plugin/telescope.lua
@@ -21,10 +21,10 @@ telescope.setup {
 require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>vk', builtin.keymaps, {})
-
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+
+vim.keymap.set('n', '<leader>vk', builtin.keymaps, {})
