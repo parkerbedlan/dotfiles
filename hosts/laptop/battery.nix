41d959704507df7cce3f78f81ef294efa452a210
Parker Bedlan <parkerbedlan@gmail.com>
2025-11-17 15:53:31 -0500

    added laptop battery settings

---
+++ hosts/laptop/battery.nix
@@ -0,0 +1,30 @@
+{
+  ...
+}:
+
+{
+  # https://nixos.wiki/wiki/Laptop
+  # Disable if devices take long to unsuspend (keyboard, mouse, etc)
+  powerManagement.powertop.enable = true;
+  services.thermald.enable = true;
+
+  services = {
+    power-profiles-daemon.enable = false;
+    tlp = {
+      enable = true;
+      settings = {
+        # set CPU_BOOST_ON_AC to 0 if it gets too hot
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        # Optional helps save long term battery health
+        START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
+        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+        # 95 for travelling
+        # STOP_CHARGE_THRESH_BAT0 = 95;
+      };
+    };
+  };
+}
--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -6,6 +6,7 @@
   imports = [
     ./hardware-configuration.nix
     ./networking.nix
+    ./battery.nix
     ../../modules/steam.nix
   ];
 
@@ -15,4 +16,16 @@
   environment.variables = {
     HOST = "laptop";
   };
+
+  # https://github.com/NixOS/nixos-hardware/blob/master/hp/laptop/14s-dq2024nf/default.nix
+  # https://github.com/NixOS/nixos-hardware/blob/master/common/pc/ssd/default.nix
+  services.fstrim.enable = true;
+
+  # TODO
+  # https://github.com/NixOS/nixos-hardware/blob/master/common/pc/default.nix
+  # boot.blacklistedKernelModules = lib.optionals (!config.hardware.enableRedistributableFirmware) [
+  #   "ath3k"
+  # ];
+  # TODO
+  # https://github.com/NixOS/nixos-hardware/blob/master/common/pc/laptop/default.nix
 }
