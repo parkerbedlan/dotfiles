a0d949f5c491d881ac5d18d5081fe02d6d752871
Parker Bedlan <parkerbedlan@gmail.com>
2026-03-25 10:02:02 -0400

    a

--- modules/nvim/after/plugin/lsp.lua
+++ modules/nvim/after/plugin/lsp.lua
@@ -33,6 +33,9 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ['<S-Tab>'] = cmp.mapping.select_prev_item(cmp_select),
         ['<Tab>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<Enter>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
         ['<C-y>'] = cmp.mapping.confirm({ select = true }),
         ['<C-Space>'] = cmp.mapping.complete(),
     }),
